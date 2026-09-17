32576d748bd6e899972507aeb7584d50befdc113
Hossammady <160312105+HossamMady@users.noreply.github.com>
2026-09-17 09:35:53 +0300

    First Day

---
+++ First_Day/Script.sh
@@ -0,0 +1,14 @@
+#! /bin/bash 
+<< 'Comment'
+this is 
+my first day 
+using bash
+Comment
+
+echo -n "Hossam"
+echo " Mady"
+echo -e "Cairo\tUniversity"
+
+echo "I'm writing from another file" > Text.txt
+echo "second line" >> Text.txt
+echo "I'm writing from another file" >> Text.txt
